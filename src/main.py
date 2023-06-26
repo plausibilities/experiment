2bae85d776f67cdefc8a117ccbeebba0e449d10a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-26 22:51:04 +0100

    Add src/main.py

---
+++ src/main.py
@@ -0,0 +1,30 @@
+"""
+The main module for running other classes
+"""
+import logging
+import os
+import sys
+
+
+def main():
+    """
+    Entry point
+    :return: None
+    """
+
+    logger.info('infections')
+
+
+if __name__ == '__main__':
+
+    # Paths
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+    main()
