0f739402ae60fcc4b85f6e7db57800bd69f2d045
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-27 05:20:03 +0100

    Update src/model/inference.py

--- src/model/inference.py
+++ src/model/inference.py
@@ -1,5 +1,4 @@
-import pymc3 as pm
-import arviz as az
+import pymc
 
 
 class Inference:
@@ -9,29 +8,23 @@ class Inference:
 
         """
 
-    def exc(self, data):
+    def exc(self, data) -> pymc.Model:
 
-        with pm.Model() as model:
+        with pymc.Model() as model:
 
             # Priors
             # A prior distribution for the
             #   residuals/noise/errors, sigma
             #   intercept, c
             #   gradient, m
-            sigma = pm.HalfCauchy(name='sigma', beta=10)
-            intercept = pm.Normal(name='intercept', mu=0, sigma=20)
-            gradient = pm.Normal(name='gradient', mu=0, sigma=20)
+            sigma = pymc.HalfCauchy(name='sigma', beta=10)
+            intercept = pymc.Normal(name='intercept', mu=0, sigma=20)
+            gradient = pymc.Normal(name='gradient', mu=0, sigma=20)
 
             # Hypothesis
-            regression = pm.Deterministic('regression', intercept + gradient * data.independent)
+            regression = pymc.Deterministic('regression', intercept + gradient * data.independent)
 
             # Define likelihood
-            likelihood = pm.Normal(name='y', mu=regression, sigma=sigma, observed=data.dependent)
+            likelihood = pymc.Normal(name='y', mu=regression, sigma=sigma, observed=data.dependent)
 
-            # Inference!
-            # draw 3000 posterior samples using NUTS sampling
-            trace = pm.sample(draws=3000, cores=2, tune=2000)
-            maximal = pm.find_MAP()
-
-            # The trace generated from MCMC sampling
-            trace = az.from_pymc3(trace=trace)
+        return model
