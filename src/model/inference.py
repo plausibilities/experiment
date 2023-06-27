fbe0212ce7eb6d6dba0126f16c7301034c39f86a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-27 09:01:20 +0100

    Update src/model/inference.py

--- src/model/inference.py
+++ src/model/inference.py
@@ -1,39 +1,42 @@
-import pymc
 import logging
-import config
+
+import arviz
+import pymc
+
+import src.model.sampling.blackjax
+import src.model.sampling.numpyro
+import src.model.sampling.simple
 
 
 class Inference:
 
-    def __init__(self):
+    def __init__(self, model: pymc.Model):
         """
 
         """
+
+        self.__model = model
 
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self, data: config.Config().DataCollection) -> pymc.Model:
+    def exc(self, sampler: str, chain_method: str = 'parallel') -> arviz.InferenceData:
+        """
+        The chain method refers to the sample drawing method.  In the case of NumPyro the options are parallel,
+        sequential, and vectorized.  Whereas, it is parallel or vectorized in the case of BlackJAX
 
-        with pymc.Model() as model:
+        * blackjax: https://www.pymc.io/projects/docs/en/stable/api/generated/pymc.sampling.jax.sample_blackjax_nuts.html
+        * numpyro: https://www.pymc.io/projects/docs/en/stable/api/generated/pymc.sampling.jax.sample_numpyro_nuts.html
 
-            # Priors
-            # A prior distribution for the
-            #   residuals/noise/errors, sigma
-            #   intercept, c
-            #   gradient, m
-            sigma = pymc.HalfCauchy(name='sigma', beta=10)
-            intercept = pymc.Normal(name='intercept', mu=0, sigma=20)
-            gradient = pymc.Normal(name='gradient', mu=0, sigma=20)
+        :param sampler:
+        :param chain_method:
+        :return:
+        """
 
-            # Hypothesis
-            # noinspection PyUnresolvedReferences
-            regression = pymc.Deterministic('regression', intercept + gradient * data.independent)
-
-            # Define likelihood
-            likelihood = pymc.Normal(name='y', mu=regression, sigma=sigma, observed=data.dependent)
-            self.__logger.info(likelihood)
-
-        return model
+        return {
+            'simple': src.model.sampling.simple.Simple().exc(model=self.__model),
+            'numpyro': src.model.sampling.numpyro.NumPyro().exc(model=self.__model, chain_method=chain_method),
+            'blackjax': src.model.sampling.blackjax.BlackJAX().exc(model=self.__model, chain_method=chain_method)
+        }.get(sampler, LookupError(f'{sampler} is not a known sampler.'))
