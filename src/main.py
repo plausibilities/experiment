1b240b06a5f1801bf86da363636f78d13f6b298e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-06-27 09:01:02 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ The main module for running other classes
 import logging
 import os
 import sys
-
+import collections
+import time
 import pymc
 
 
@@ -16,8 +17,21 @@ def main():
 
     logger.info('experiment')
 
+    # Sample data
     data: config.Config().DataCollection = src.data.points.Points().exc()
-    model: pymc.Model = src.model.inference.Inference().exc(data=data)
+
+    # The suggested model
+    model: pymc.Model = src.model.algorithm.Algorithm().exc(data=data)
+
+    # The inference instance
+    inference = src.model.inference.Inference(model=model)
+
+    # Estimating the model's parameters via different sampling methods
+    for option in options:
+        logger.info('%s ...', option.sampler)
+        starts = time.time()
+        inference.exc(sampler=option.sampler, chain_method=option.chain_method)
+        logger.info('%s: %s', (option.sampler, time.time() - starts))
 
 
 if __name__ == '__main__':
@@ -36,6 +50,16 @@ if __name__ == '__main__':
     # Classes
     import config
     import src.data.points
+    import src.model.algorithm
     import src.model.inference
+
+    # The inference options
+    Case = collections.namedtuple(typename='Case', field_names=['sampler', 'chain_method'])
+    cases = [{'sampler': 'simple', 'chain_method': ''},
+             {'sampler': 'numpyro', 'chain_method': 'vectorized'},
+             {'sampler': 'blackjax', 'chain_method': 'vectorized'},
+             {'sampler': 'numpyro', 'chain_method': 'parallel'},
+             {'sampler': 'blackjax', 'chain_method': 'parallel'}]
+    options = [Case(**case) for case in cases]
 
     main()
