c9b7ad77cf8ee0b7c7752653d28ba6d8bf101a52
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-14 20:51:43 +0100

    Update src/model/sampling/numpyro.py

--- src/model/sampling/numpyro.py
+++ src/model/sampling/numpyro.py
@@ -35,6 +35,7 @@ class NumPyro:
         print(method)
 
         with model:
+            # Inference
             trace = pymc.sampling_jax.sample_numpyro_nuts(
                 draws=2000, tune=1000, chains=4, target_accept=0.9,
                 random_seed=self.random_seed, chain_method=method)
