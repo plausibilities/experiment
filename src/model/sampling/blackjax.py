0865d7a182e7ba3f8de9e535d65791b0ccd2f2ae
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-08-14 20:51:46 +0100

    Update src/model/sampling/blackjax.py

--- src/model/sampling/blackjax.py
+++ src/model/sampling/blackjax.py
@@ -35,7 +35,7 @@ class BlackJAX:
         print(method)
 
         with model:
-
+            # Inference
             trace = pymc.sampling_jax.sample_blackjax_nuts(
                 draws=2000, tune=1000, chains=4, target_accept=0.9,
                 random_seed=self.random_seed, chain_method=method)
