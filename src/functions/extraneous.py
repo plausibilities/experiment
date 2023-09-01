7929864750bf4edd92aa8860f382ca7e5386367c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-01 06:29:15 +0100

    Add src/functions/extraneous.py

---
+++ src/functions/extraneous.py
@@ -0,0 +1,30 @@
+import pathlib
+import shutil
+import logging
+
+class Extraneous:
+
+    def __init__(self) -> None:
+        """
+        
+        """
+        
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def exc(self):
+        """
+        Deletes all __pycache__ directories
+        """
+
+        for path in pathlib.Path.cwd().rglob('__pycache__'):
+            if path.is_dir():
+                try:
+                    shutil.rmtree(path)
+                except PermissionError:
+                    raise Exception(f'Delete Permission Denied: {path}')
+                else:
+                    self.__logger.info(f'Deleted: {path}')
