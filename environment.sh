38740a7f3b7ba7e7caa572de991e49eeb8401c33
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-02 17:45:34 +0000

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
 # Create virtual environment <tensors>
 conda env create -f environment.yml -p /opt/miniconda3/envs/uncertainty
-
 
 # Activate <tensors>
 conda activate uncertainty
