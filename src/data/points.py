805c5073382d45770094fd2eb74786fcc353c160
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-28 15:23:32 +0100

    Update src/data/points.py

--- src/data/points.py
+++ src/data/points.py
@@ -1,24 +1,22 @@
-import collections
-
+"""Module points.py"""
 import numpy as np
 
-import config
+import src.elements.parameters as pr
+import src.elements.points as pi
 
 
 class Points:
+    """
+    Builds a simple set of data points
+    """
 
     def __init__(self):
         """
-
+        Constructor
         """
 
-        configurations = config.Config()
-        self.__DataCollection = configurations.DataCollection
-
         # Parameters for data generation
-        Parameters = collections.namedtuple(typename='Parameters',
-                                            field_names=['T', 'N', 'intercept', 'gradient', 'noise_loc', 'noise_scale'])
-        self.parameters = Parameters(T=600, N=500, intercept=1.5, gradient=2.5, noise_loc=0.0, noise_scale=0.5)
+        self.parameters = pr.Parameters
 
     def __model(self) -> (np.ndarray, np.ndarray):
         """
@@ -26,7 +24,7 @@ class Points:
         :return:
         """
 
-        abscissae = np.linspace(start=0, stop=2, num=self.parameters.T)
+        abscissae = np.linspace(start=0, stop=2, num=self.parameters.n_instances)
         abscissae = np.expand_dims(abscissae, axis=1)
 
         ordinates = (self.parameters.gradient * abscissae) + self.parameters.intercept
@@ -44,16 +42,16 @@ class Points:
 
         # Noise
         noise = np.random.normal(
-            loc=self.parameters.noise_loc, scale=self.parameters.noise_scale, size=self.parameters.N)
+            loc=self.parameters.noise_location, scale=self.parameters.noise_scale, size=self.parameters.n_excerpt)
         noise = np.expand_dims(noise, axis=1)
 
         # The Measures
-        independent = abscissae[:self.parameters.N]
-        dependent = ordinates[:self.parameters.N] + noise[:self.parameters.N]
+        independent = abscissae[:self.parameters.n_excerpt]
+        dependent = ordinates[:self.parameters.n_excerpt] + noise[:self.parameters.n_excerpt]
 
         return independent, dependent
 
-    def exc(self) -> config.Config().DataCollection:
+    def exc(self) -> pi.Points:
         """
 
         :return:
@@ -62,5 +60,5 @@ class Points:
         abscissae, ordinates = self.__model()
         independent, dependent = self.__measures(abscissae=abscissae, ordinates=ordinates)
 
-        return self.__DataCollection(abscissae=abscissae, ordinates=ordinates,
-                                     independent=independent, dependent=dependent)
+        return pi.Points(abscissae=abscissae, ordinates=ordinates,
+                         independent=independent, dependent=dependent)
