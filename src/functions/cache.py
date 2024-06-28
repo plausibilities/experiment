282e00564eab4dfd73268d4c0cf5a830aea72078
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-28 14:33:05 +0100

    Update src/functions/cache.py

--- src/functions/cache.py
+++ src/functions/cache.py
@@ -7,28 +7,43 @@ import shutil
 
 
 class Cache:
+    """
+    Extraneous
+    """
 
     def __init__(self) -> None:
         """
-        
+        Constructor
         """
-        
+
+        self.__patterns: list[str] = ['__pycache__', '.pytest_cache']
+
         # Logging
-        logging.basicConfig(level=logging.INFO,
+        logging.basicConfig(level=logging.WARNING,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self):
-        """
-        Deletes all __pycache__ directories
+    def __delete(self, pattern: str) -> None:
         """
 
-        for path in pathlib.Path.cwd().rglob('__pycache__'):
+        :return:
+        """
+
+        for path in pathlib.Path.cwd().rglob(pattern=pattern):
             if path.is_dir():
                 try:
-                    shutil.rmtree(path=path, ignore_errors=True)
-                except PermissionError:
-                    raise Exception(f'Delete Permission Denied: {path}')
-                else:
-                    self.__logger.info(f'Deleted: {path}')
+                    shutil.rmtree(path)
+                except PermissionError as err:
+                    raise (self.__logger.warning(err)) from err
+
+    def exc(self):
+        """
+
+        :return: None
+        """
+
+        for pattern in self.__patterns:
+
+            self.__delete(pattern=pattern)
+
