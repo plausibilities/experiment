7ee80a5a53ce5f995aedba21451a9f34035941b0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-06-29 22:34:07 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -16,27 +16,31 @@ def main():
     :return: None
     """
 
+    logger = logging.getLogger(__name__)
+
     # Notes
     logger.info('JAX')
     logger.info(jax.devices(backend='gpu'))
-    logger.info(f"The number of GPU devices: {jax.device_count(backend='gpu')}")
-    
+    logger.info('The number of GPU devices: %s', jax.device_count(backend='gpu'))
+
     # Sample data
     data: pi.Points  = src.data.points.Points().exc()
-    
+
     # The suggested model
     model: pymc.Model = src.model.algorithm.Algorithm().exc(data=data)
 
+    # Setting up for inference
+    sampling = src.elements.sampling.Sampling(chains=8)
+
     # Inference
-    sampling = smp.Sampling(chains=8)
     interface = src.model.inference.Inference(sampling=sampling)
     estimates: arviz.InferenceData = interface.exc(
         model=model, nuts_sampler='numpyro', chain_method='vectorized')
     logger.info(estimates.__dict__)
 
-
     # Delete __pycache__ directories
     src.functions.cache.Cache().exc()
+
 
 if __name__ == '__main__':
 
@@ -55,12 +59,11 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    logger = logging.getLogger(__name__)
 
     # Classes
     import src.data.points
     import src.elements.points as pi
-    import src.elements.sampling as smp
+    import src.elements.sampling
     import src.functions.cache
     import src.model.algorithm
     import src.model.inference
